47aaa1759ac3eeecb5fa4206042d718bbdc1937f
Guilherme Souza <dev.guilhermesouza@gmail.com>
2021-11-01 11:53:44 -0300

    Initial commit

---
+++ studio/schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'color',
+      title: 'Color',
+      type: 'color'
+    }
+  ],
+}
